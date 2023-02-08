fd4301957863f7755df95a8052488b736b790460
sophharris1998 <117087805+sophharris1998@users.noreply.github.com>
2023-02-08 17:06:17 +0000

    two test pass

--- untitled/src/BankAccountTest.java
+++ untitled/src/BankAccountTest.java
@@ -1,18 +1,29 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-class BankAccountTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
+class BankAccountTest {
+    private BankAccount account;
     @BeforeEach
+        //Arrange
     void setUp() {
+        account = new BankAccount (1000.00, "Sophie Account");
+
     }
 
     @Test
     void deposit() {
+        //Act
+        account.deposit(200);
+        //Assert
+        assertEquals(1200.00, account.getBalance());
     }
 
     @Test
     void withdraw() {
+        account.withdraw(200);
+        assertEquals(800.00, account.getBalance() );
     }
 }
